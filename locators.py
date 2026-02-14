6d4c35eb8e91ee21dd2778aa9ad2463c0a7aaf7d
rgn2836 <bublik0706@gmail.com>
2026-02-14 17:14:46 +0300

    Написан тест test_login_from_recovery_password_form

--- locators.py
+++ locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 class GeneralLocators:
     HEADER = By.XPATH, '//header'
+    PERSONAL_ACCOUNT_BUTTON = By.XPATH, '//a[@href="/account"]'
 
 class RegistrationFormLocators:
     NAME_INPUT = By.XPATH, '//label[text()="Имя"]/following::input[@class="text input__textfield text_type_main-default"]'
@@ -9,6 +10,7 @@ class RegistrationFormLocators:
     PASSWORD_INPUT = By.XPATH, '//label[text()="Пароль"]/following::input[@class="text input__textfield text_type_main-default"]'
     REGISTRATION_BUTTON = By.XPATH, '//button[text()="Зарегистрироваться"]'
     PASSWORD_ERROR_ALERT = By.XPATH, '//p[@class="input__error text_type_main-default"]'
+    LOGIN_BUTTON = By.XPATH, '//a[@href="/login"]'
 
 class LoginFormLocators:
     EMAIL_INPUT = By.XPATH, '//label[text()="Email"]/following::input[@class="text input__textfield text_type_main-default"]'
@@ -19,3 +21,6 @@ class MainPageLocators:
     LOGIN_ACCOUNT_BUTTON = By.XPATH, '//button[text()="Войти в аккаунт"]'
     PLACE_ORDER_BUTTON = By.XPATH, '//button[text()="Оформить заказ"]'
     
+class PasswordRecoveryFormLocators:
+    LOGIN_BUTTON = By.XPATH, '//a[@href="/login"]'
+    RECOVERY_BUTTON = By.XPATH, '//button[text()="Восстановить"]'
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,5 @@
 from tools.generator import generate_email, generate_valid_password, generate_wrong_password_with_1_symbol, generate_wrong_password_with_4_symbols, generate_wrong_password_with_5_symbols
-from locators import GeneralLocators, RegistrationFormLocators, LoginFormLocators, MainPageLocators
+from locators import GeneralLocators, RegistrationFormLocators, LoginFormLocators, MainPageLocators, PasswordRecoveryFormLocators
 
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
@@ -30,6 +30,117 @@ def test_login_from_login_button(driver):
     login_main_page_button = driver.find_element(*MainPageLocators().LOGIN_ACCOUNT_BUTTON)
     login_main_page_button.click()
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    email_authorization_form_input = driver.find_element(*LoginFormLocators().EMAIL_INPUT)
+    email_authorization_form_input.send_keys(email)
+
+    password_authorization_input = driver.find_element(*LoginFormLocators().PASSWORD_INPUT)
+    password_authorization_input.send_keys(password)
+
+    login_authorization_form_button = driver.find_element(*LoginFormLocators().LOGIN_BUTTON)
+    login_authorization_form_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(MainPageLocators().PLACE_ORDER_BUTTON))
+
+    assert driver.current_url == "https://stellarburgers.education-services.ru/"
+
+def test_login_from_personal_account_button(driver):
+    driver.get("https://stellarburgers.education-services.ru/register")
+    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(GeneralLocators().HEADER))
+
+    name_input = driver.find_element(*RegistrationFormLocators().NAME_INPUT)
+    name_input.send_keys('Antonio')
+
+    email_input = driver.find_element(*RegistrationFormLocators().EMAIL_INPUT)
+    email = generate_email()
+    email_input.send_keys(email)
+
+    password_input = driver.find_element(*RegistrationFormLocators().PASSWORD_INPUT)
+    password = generate_valid_password()
+    password_input.send_keys(password)
+
+    registration_button = driver.find_element(*RegistrationFormLocators().REGISTRATION_BUTTON)
+    registration_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    personal_account_button = driver.find_element(*GeneralLocators().PERSONAL_ACCOUNT_BUTTON)
+    personal_account_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    email_authorization_form_input = driver.find_element(*LoginFormLocators().EMAIL_INPUT)
+    email_authorization_form_input.send_keys(email)
+
+    password_authorization_input = driver.find_element(*LoginFormLocators().PASSWORD_INPUT)
+    password_authorization_input.send_keys(password)
+
+    login_authorization_form_button = driver.find_element(*LoginFormLocators().LOGIN_BUTTON)
+    login_authorization_form_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(MainPageLocators().PLACE_ORDER_BUTTON))
+
+    assert driver.current_url == "https://stellarburgers.education-services.ru/"
+
+def test_login_from_registration_form_login_button(driver):
+    driver.get("https://stellarburgers.education-services.ru/register")
+    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(GeneralLocators().HEADER))
+
+    name_input = driver.find_element(*RegistrationFormLocators().NAME_INPUT)
+    name_input.send_keys('Antonio')
+
+    email_input = driver.find_element(*RegistrationFormLocators().EMAIL_INPUT)
+    email = generate_email()
+    email_input.send_keys(email)
+
+    password_input = driver.find_element(*RegistrationFormLocators().PASSWORD_INPUT)
+    password = generate_valid_password()
+    password_input.send_keys(password)
+
+    registration_button = driver.find_element(*RegistrationFormLocators().REGISTRATION_BUTTON)
+    registration_button.click()
+    WebDriverWait(driver, 8).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    driver.get("https://stellarburgers.education-services.ru/register")
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(RegistrationFormLocators().REGISTRATION_BUTTON))
+
+    login_registration_form_button = driver.find_element(*RegistrationFormLocators().LOGIN_BUTTON)
+    login_registration_form_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    email_authorization_form_input = driver.find_element(*LoginFormLocators().EMAIL_INPUT)
+    email_authorization_form_input.send_keys(email)
+
+    password_authorization_input = driver.find_element(*LoginFormLocators().PASSWORD_INPUT)
+    password_authorization_input.send_keys(password)
+
+    login_authorization_form_button = driver.find_element(*LoginFormLocators().LOGIN_BUTTON)
+    login_authorization_form_button.click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(MainPageLocators().PLACE_ORDER_BUTTON))
+
+    assert driver.current_url == "https://stellarburgers.education-services.ru/"
+
+def test_login_from_recovery_password_form(driver):
+    driver.get("https://stellarburgers.education-services.ru/register")
+    WebDriverWait(driver, 3).until(EC.visibility_of_element_located(GeneralLocators().HEADER))
+
+    name_input = driver.find_element(*RegistrationFormLocators().NAME_INPUT)
+    name_input.send_keys('Antonio')
+
+    email_input = driver.find_element(*RegistrationFormLocators().EMAIL_INPUT)
+    email = generate_email()
+    email_input.send_keys(email)
+
+    password_input = driver.find_element(*RegistrationFormLocators().PASSWORD_INPUT)
+    password = generate_valid_password()
+    password_input.send_keys(password)
+
+    registration_button = driver.find_element(*RegistrationFormLocators().REGISTRATION_BUTTON)
+    registration_button.click()
+    WebDriverWait(driver, 8).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
+
+    driver.get("https://stellarburgers.education-services.ru/forgot-password")
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(PasswordRecoveryFormLocators().RECOVERY_BUTTON))
+
+    login_recovery_password_form = driver.find_element(*PasswordRecoveryFormLocators().LOGIN_BUTTON)
+    login_recovery_password_form.click()
+    WebDriverWait(driver, 8).until(EC.visibility_of_element_located(LoginFormLocators().LOGIN_BUTTON))
 
     email_authorization_form_input = driver.find_element(*LoginFormLocators().EMAIL_INPUT)
     email_authorization_form_input.send_keys(email)
